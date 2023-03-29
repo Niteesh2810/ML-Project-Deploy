4a00e902f5db6f0466d99ecfa0f95c7cc95c8bc6
Niteesh <niteeshm707@gmail.com>
2023-03-29 23:45:42 +0530

    app

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
-import pickle 
-from flask import Flask,request,app,jsonify,url_for,render_template
-
+from flask import Flask, render_template, request
+import pickle
 import numpy as np
 import pandas as pd
 
-app=Flask(__name__)
-model=pickle.load(open('model.pkl','rb'))
+app = Flask(__name__, template_folder='templates')
+model = pickle.load(open('model2.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route('/predict_api',method=["POST"])
-def predict_api():
-    data=request.json['data']
-    output=model.predict(np.array(list(data.values())).reshape(1,-1))
-    return jsonify(output[0])
+@app.route('/predict', methods=["POST"])
+def predict():
+    Age = int(request.form.get('age'))
+    Workclass = request.form.get('workclass')
+    Education = request.form.get('education')
+    marital_status = request.form.get('marital_status')
+    occupation = request.form.get('occupation')
+    sex = request.form.get('sex')
+    hours_per_week = int(request.form.get('hours_per_week'))
+    col=["Age", "hours_per_week", "Workclass", "Education", "marital_status", "occupation", "sex"]
+    output = model.predict(pd.DataFrame(np.array([Age, hours_per_week, Workclass, Education, marital_status, occupation, sex]).reshape(1, 7),columns=col))[0]
+    if output == 1:
+        result = "INCOME IS >=50K"
+    else:
+        result = "INCOME IS <=50K"
+    return render_template('index.html', result=result)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
